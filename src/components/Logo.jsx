e999a053a578a05f69b55ad60e441853fb8a6e09
Ayush rawat <165577927+Ayu-Rawat@users.noreply.github.com>
2024-12-31 17:11:32 +0530

    debugging

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ function Logo({ width = '80px' }) {
     return (
         <div className="flex items-center justify-center">
             <div className="text-xl font-bold text-blue-600" style={{ width }}>
-                <img src="/logo.png" alt="logo" />
+                <img src="../images/logo.png" alt="logo" />
             </div>
         </div>
     );
